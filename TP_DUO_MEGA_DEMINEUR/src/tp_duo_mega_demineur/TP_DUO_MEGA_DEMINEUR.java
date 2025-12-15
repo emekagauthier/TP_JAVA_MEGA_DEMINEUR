f15b9da0e8e863a8939d8950defc1cd9a3232a88
emeka <emeka.gauthier@epfedu.fr>
2025-12-15 16:21:10 +0100

    MINI Projet : Mega Cadenas (Start)
    Création fichier

---
+++ TP_DUO_MEGA_DEMINEUR/src/tp_duo_mega_demineur/TP_DUO_MEGA_DEMINEUR.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp_duo_mega_demineur;
+
+/**
+ *
+ * @author emeka
+ */
+public class TP_DUO_MEGA_DEMINEUR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
