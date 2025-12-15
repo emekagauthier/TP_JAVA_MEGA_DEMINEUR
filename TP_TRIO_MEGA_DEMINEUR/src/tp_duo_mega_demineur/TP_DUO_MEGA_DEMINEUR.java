c896386fdc459a2d920982cc39953f61fe7a125b
rlignon <ronan.lignon@epfedu.fr>
2025-12-15 16:35:54 +0100

    teste commit

--- TP_TRIO_MEGA_DEMINEUR/src/tp_duo_mega_demineur/TP_DUO_MEGA_DEMINEUR.java
+++ TP_TRIO_MEGA_DEMINEUR/src/tp_duo_mega_demineur/TP_DUO_MEGA_DEMINEUR.java
@@ -14,7 +14,7 @@ public class TP_DUO_MEGA_DEMINEUR {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int test;
     }
     
 }
