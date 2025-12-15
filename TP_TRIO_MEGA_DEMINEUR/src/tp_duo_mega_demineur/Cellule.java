18d35acd9a321e26a52c86cc39625adcfde99b90
emeka <emeka.gauthier@epfedu.fr>
2025-12-15 17:14:01 +0100

    MINI Projet : Mega DEMINEUR Classe Cellule
    Initialisation de la classe Cellule
    attribue et methode

--- TP_TRIO_MEGA_DEMINEUR/src/tp_duo_mega_demineur/Cellule.java
+++ TP_TRIO_MEGA_DEMINEUR/src/tp_duo_mega_demineur/Cellule.java
@@ -9,5 +9,36 @@ package tp_duo_mega_demineur;
  * @author emeka
  */
 public class Cellule {
-    
+// Attribut principaux Class Cellule 
+private boolean Presence_Bombe;
+private boolean devoilee ;
+private int nbBombesAdjacentes ;
+
+public boolean getPresenceBombe(){
+    return Presence_Bombe;
+}
+
+public int getNbBombesAdjacentes(){
+    return nbBombesAdjacentes;
+}
+
+//Méthode
+public void placerBombe(){
+    Presence_Bombe=true;
+}
+public void revelerCellule(){
+    devoilee=true;
+}
+
+    @Override
+    public String toString() {
+        if (devoilee==false){
+            return "?";
+        }
+        if (Presence_Bombe==true && devoilee==true ){
+            return "B";
+        }
+        if()
+    }
+
 }
