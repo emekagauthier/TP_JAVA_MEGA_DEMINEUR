041f8fcd7447e224fdab72698d5defed7baaed66
rlignon <ronan.lignon@epfedu.fr>
2025-12-17 10:06:51 +0100

    TP_Demineur_Class_GrilleDeJeu, Ronan >Methode placerBombesAleatoirement()

--- TP_TRIO_MEGA_DEMINEUR/src/tp_duo_mega_demineur/GrilleDeJeu.java
+++ TP_TRIO_MEGA_DEMINEUR/src/tp_duo_mega_demineur/GrilleDeJeu.java
@@ -9,7 +9,8 @@ package tp_duo_mega_demineur;
  * @author emeka
  */
 public class GrilleDeJeu {
-    private Cellule [][] grille = new Cellule [10][10];
+
+    private Cellule[][] grille = new Cellule[10][10];
     private int nbLignes;
     private int nbColonnes;
     private int nbBombes;
@@ -25,8 +26,18 @@ public class GrilleDeJeu {
     public int getNbBombes() {
         return nbBombes;
     }
-    
-    
-    
-    
+
+    public void placerBombesAleatoirement() {
+        int compteur = 1;
+        while (compteur != nbBombes) {
+            int i = (int) (Math.random() * nbColonnes);
+            int j = (int) (Math.random() * nbLignes);
+            while (grille[i][j].getPresenceBombe()==true) {
+                i = (int)(Math.random() * nbColonnes);
+                j = (int) (Math.random() * nbLignes);
+            }
+            grille[i][j].placerBombe();
+        }
+
+    }
 }
