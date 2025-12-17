ef489e938c8d9e183511a1051e534d028a4571ba
emeka <emeka.gauthier@epfedu.fr>
2025-12-17 12:44:17 +0100

    MINI Projet : Mega DEMINEUR 	Fichier Class metier et Class Partie

---
+++ TP_TRIO_MEGA_DEMINEUR/src/Class_Metier/Partie.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class_Metier;
+
+/**
+ *
+ * @author emeka
+ */
+public class Partie {
+//Attributs
+GrilleDeJeu partie ;
+
+public void initialiserPartie(){
+    partie.placerBombesAleatoirement();
+    partie.calculerBombesAdjacentes();
+}
+public void tourDeJeu(int ligne, int colonne){
+    partie.revelerCellule(ligne, colonne);
+    
+    
+    
+}
+public String verifierVictoire(){
+    if(partie.toutesCellulesRevelees()==true){
+        return "Victoire";
+    }
+    else{
+        return 
+    }
+        
+}
+
+public void StartPartie(){
+    initialiserPartie();
+    
+    
+    
+}
+
+
+}
